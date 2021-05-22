3f6badd2fa25ea9c599235df05c635b13c793ae0
hcwnbs <280497626@qq.com>
2021-05-22 23:28:38 +0800

    commit 1 solution

--- Leetcode/Go/twoSum.go
+++ Leetcode/Go/twoSum.go
@@ -1,5 +1,17 @@
 package twoSum
 
-func twoSum() {
+/*
+时间复杂度：O(N)
+空间复杂度：O(N)
 
+*/
+func twoSum(nums []int, target int) []int {
+	hashTable := map[int]int{}
+	for i, x := range nums{
+		if j, ok := hashTable[target - x]; ok{
+			return []int{j, i}
+		}
+		hashTable[x] = i
+	}
+	return nil
 }
